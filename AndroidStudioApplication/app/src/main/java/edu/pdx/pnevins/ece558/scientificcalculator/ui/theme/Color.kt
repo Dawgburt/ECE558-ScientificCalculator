d467c3c4ef8118e8658a8eea340ec5299f529bc1
PNevins971 <pnevins@pdx.edu>
2025-02-08 12:12:37 -0800

    I turned my experiment calculator into the Project 1 Scientific Calculator. First upload

---
+++ AndroidStudioApplication/app/src/main/java/edu/pdx/pnevins/ece558/scientificcalculator/ui/theme/Color.kt
@@ -0,0 +1,15 @@
+package edu.pdx.pnevins.ece558.scientificcalculator.ui.color
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+val DarkBlue =  Color(0xFF00008B) // Dark Blue
+val DarkerBlue =  Color(0xFF000066) // Darker Navy Blue
+val BloodRed = Color(0xFF8A0707) // Blood Red
