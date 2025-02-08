b3d9e1b281312a48060cc492d63383f8e3c2c51d
PNevins971 <pnevins@pdx.edu>
2025-02-08 14:42:18 -0800

    Added headers - stable version - last one destablized somehow

---
+++ AndroidStudioAppV2/app/src/main/java/edu/pdx/pnevins/ece558/scientificcalculatorv2/ui/theme/Color.kt
@@ -0,0 +1,30 @@
+/**
+ * Color.kt
+ *
+ * ECE 558 Scientific Calculator
+ *
+ * Defines the color scheme used in the calculator application.
+ * Includes primary, secondary, and custom colors for UI consistency.
+ *
+ * Developed for ECE 558 at Portland State University.
+ *
+ * Author: Phil Nevins (p.nevins971@gmail.com)
+ * Date: 2/8/2025
+ */
+
+
+package edu.pdx.pnevins.ece558.scientificcalculator.ui.color
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+val DarkBlue =  Color(0xFF00008B) // Dark Blue
+val DarkerBlue =  Color(0xFF000066) // Darker Navy Blue
+val BloodRed = Color(0xFF8A0707) // Blood Red
---
+++ AndroidStudioAppV2/app/src/main/java/edu/pdx/pnevins/ece558/scientificcalculatorv2/ui/theme/Type.kt
@@ -0,0 +1,33 @@
+/**
+ * Type.kt
+ *
+ * ECE 558 Scientific Calculator
+ *
+ * Defines the typography styles used in the calculator application.
+ * Customizes text appearance using Jetpack Compose's Material3 Typography.
+ *
+ * Developed for ECE 558 at Portland State University.
+ *
+ * Author: Phil Nevins (p.nevins971@gmail.com)
+ * Date: 2/8/2025
+ */
+
+
+package edu.pdx.pnevins.ece558.scientificcalculator.ui.theme
+
+import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
+// Set of Material typography styles to start with
+val Typography = Typography(
+    bodyLarge = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.5.sp
+    )
+)
